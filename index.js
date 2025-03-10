54257c85a041704012d0fa62eb252fcc767f640f
vaishnavi640 <vaishnavikalluri@gmail.com>
2025-03-10 16:40:42 +0530

    changes

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const { resolve } = require('path');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+// Load environment variables from .env file
+dotenv.config();
 
 const app = express();
-const port = 3010;
+const port = process.env.PORT || 3010;
 
 app.use(express.static('static'));
 
@@ -10,6 +15,14 @@ app.get('/', (req, res) => {
   res.sendFile(resolve(__dirname, 'pages/index.html'));
 });
 
+// MongoDB connection
+mongoose.connect(process.env.MONGO_URI, {
+    // useNewUrlParser: true,
+    // useUnifiedTopology: true,
+})
+.then(() => console.log('Connected to database'))
+.catch((error) => console.error(`Error connecting to database: ${error}`));
+
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+  console.log(`Server is running at http://localhost:${port}`);
 });
